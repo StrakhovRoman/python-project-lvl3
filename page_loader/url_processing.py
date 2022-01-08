6110368efb5e8ef29758a81e9c6c7fb1cc7b8e73
Roman Strakhov <rvstrakhov@gmail.com>
2022-01-08 17:38:53 +0300

    add url_processing.py

---
+++ page_loader/url_processing.py
@@ -0,0 +1,66 @@
+"""URL processing."""
+
+import logging.config  # noqa: WPS301
+from urllib.parse import urlparse
+
+import requests
+
+from page_loader.logger_config import logger_config
+
+logging.config.dictConfig(logger_config)
+logger = logging.getLogger('app_logger')
+
+CHUNK_SIZE = 128
+
+
+def get_response(url, content_type='text'):
+    try:  # noqa: WPS229
+        response = requests.get(url)
+        response.raise_for_status()
+
+    except requests.exceptions.ConnectionError as error:
+        logger.exception(error)
+        raise SystemExit(
+            'Connection error! Check your network connection and try again.',
+        )
+
+    except requests.exceptions.HTTPError as error:
+        logger.exception(error)
+        raise SystemExit(error)
+
+    except requests.exceptions.RequestException as error:
+        logger.exception(error)
+        raise SystemExit(
+            f'Something went wrong while loading page: {url}',
+        )
+    if content_type == 'text':
+        return response.text
+    if content_type == 'content':
+        return get_chunk(response)
+
+
+def get_chunk(response):
+    try:
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            yield chunk
+
+    except requests.exceptions.RequestException as error:
+        logger.exception(error)
+        raise SystemExit(
+            f'Can not download resource {response.url}',
+        )
+
+
+def get_url_from_local_link(page_url, link):
+    """Get URL from local resources link."""
+    link = link.lstrip('.')
+    scheme = urlparse(page_url).scheme
+    netloc = urlparse(page_url).netloc
+    path = urlparse(link).path
+    return f'{scheme}://{netloc}{path}'
+
+
+def is_local_resource(page_url, link):
+    link_netloc = urlparse(link).netloc
+    page_netloc = urlparse(page_url).netloc
+    return link_netloc == page_netloc or not link_netloc
