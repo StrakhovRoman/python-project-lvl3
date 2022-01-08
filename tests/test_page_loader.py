062163999b46b712d0e01d988ef6b771c2d3dcc4
Roman Strakhov <rvstrakhov@gmail.com>
2022-01-08 17:50:18 +0300

    change test_page_loader.py

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,14 +1,18 @@
 """Test page loader."""
 import tempfile
 
+import pytest
 import requests_mock
 
-from page_loader.page_loader import download, get_file_name
+from page_loader.loader import download
+from page_loader.naming import get_directory_name, get_file_name
+from page_loader.url_processing import get_url_from_local_link
 
 TEST_URL = 'https://ru.hexlet.io/courses'
-GENERATED_FILENAME = 'ru-hexlet-io-courses.html'
+FILE_NAME = 'ru-hexlet-io-courses.html'
+DIRECTORY_NAME = 'ru-hexlet-io-courses_files'
 
-
+'''
 def test_download():
     with tempfile.TemporaryDirectory() as temporary_directory:
         with requests_mock.Mocker() as mocker:
@@ -17,7 +21,33 @@ def test_download():
             with open(file_path, 'r') as file:  # noqa: WPS110
                 page = file.read()
                 assert page == 'testing_page'
+'''
 
 
 def test_get_file_name():
-    assert GENERATED_FILENAME == get_file_name(TEST_URL)
+    assert FILE_NAME == get_file_name(TEST_URL)
+
+
+def test_get_directory_name():
+    assert DIRECTORY_NAME == get_directory_name(TEST_URL)
+
+
+@pytest.mark.parametrize(
+    'link, correct_value',
+    [
+        (
+            '/assets/application.css',
+            'https://ru.hexlet.io/assets/application.css',
+        ),
+        (
+            '/courses',
+            'https://ru.hexlet.io/courses',
+        ),
+        (
+            '/assets/professions/nodejs.png',
+            'https://ru.hexlet.io/assets/professions/nodejs.png',
+        ),
+    ],
+)
+def test_get_url_from_local_link(link, correct_value):
+    assert get_url_from_local_link(TEST_URL, link) == correct_value
