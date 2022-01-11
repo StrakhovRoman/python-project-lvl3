66352ed7c06b199b279096c2cbb87e71466306cf
Roman Strakhov <rvstrakhov@gmail.com>
2022-01-11 23:15:31 +0300

    remove NAME constant

--- page_loader/naming.py
+++ page_loader/naming.py
@@ -4,8 +4,6 @@ import os
 import re
 from urllib.parse import urlparse
 
-NAME = '{0}{1}'
-
 
 def get_name(page_url, link=False, directory=False):  # noqa: WPS210
     """Get a file or directory name from page URL.
@@ -22,7 +20,7 @@ def get_name(page_url, link=False, directory=False):  # noqa: WPS210
     else:
         path_parts = urlparse(page_url).path
         extension = '.html'
-    name = NAME.format(domain_name, path_parts.rstrip('/'))
+    name = '{0}{1}'.format(domain_name, path_parts.rstrip('/'))
     if directory:
         return convert_name(name)
     return convert_name(name), extension
@@ -33,8 +31,8 @@ def convert_name(resource):
 
 
 def get_file_name(page_url, source_link=False):
-    return NAME.format(*get_name(page_url, link=source_link))
+    return '{0}{1}'.format(*get_name(page_url, link=source_link))
 
 
 def get_directory_name(page_url):
-    return NAME.format(get_name(page_url, directory=True), '_files')
+    return '{0}{1}'.format(get_name(page_url, directory=True), '_files')
