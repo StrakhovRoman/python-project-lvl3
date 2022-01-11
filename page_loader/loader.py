31c22c86ba52836c0b80a28290458c7ee25eaa8c
Roman Strakhov <rvstrakhov@gmail.com>
2022-01-11 21:26:56 +0300

    change logging in loader.py

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -3,30 +3,28 @@
 import logging.config  # noqa: WPS301
 
 from page_loader import data_processing, naming
-from page_loader.logger_config import logger_config
+from page_loader.logger_config import configuring_dict
 
-logging.config.dictConfig(logger_config)
+logging.config.dictConfig(configuring_dict)
 logger = logging.getLogger('app_logger')
 
 
 def download(page_url: str, output_path: str) -> str:
 
-    logger.info('make soup')
-
     soup = data_processing.get_soup(page_url)
 
-    logger.info('make path with file name')
+    logger.info('soup done')
 
     path_to_file = data_processing.make_path(
         output_path,
         naming.get_file_name(page_url),
     )
 
-    logger.info('make path and dir for resource')
+    logger.info('path with file name done')
 
     path_to_resource_dir = data_processing.make_dir(page_url, output_path)
 
-    logger.info('changing links')
+    logger.info('path and dir for downloadable resources done')
 
     print(f'Loading to {path_to_resource_dir}:\n')  # noqa: WPS421
 
@@ -36,7 +34,10 @@ def download(page_url: str, output_path: str) -> str:
         path_to_resource_dir,
     )
 
-    logger.info('saving page with new local links')
+    logger.info('change links done')
 
     data_processing.save_page(path_to_file, html_with_local_links)
+
+    logger.info('saving page with new local links done')
+
     return path_to_file
