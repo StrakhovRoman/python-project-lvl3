9c42bab6a4b090ccf9d573bb74aa444a291b1395
Roman Strakhov <rvstrakhov@gmail.com>
2022-01-31 22:46:32 +0300

    update main function

--- page_loader/scripts/main.py
+++ page_loader/scripts/main.py
@@ -10,9 +10,13 @@ from page_loader.loader import download
 def main():
     args = get_parser().parse_args()
     try:
-        print(download(args.url, args.output))
+        page = download(args.url, args.output)
     except Exception as error:
         print(error)
+
+    print()
+    print('Page is completely loaded into:')
+    print(page)
 
 
 if __name__ == '__main__':
